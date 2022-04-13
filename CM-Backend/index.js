8a1d82323cbb593c1aed4fc990d9ced025e664c2
vexagonverp <verproti80@yahoo.com.vn>
2022-04-13 21:03:41 +0700

    Format code and socket Io

--- CM-Backend/index.js
+++ CM-Backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser');
-const { dataService} = require(path.join(process.cwd(),'src','service','data.service'));
+const { Server } = require("socket.io");
+const { dataService } = require(path.join(process.cwd(), 'src', 'service', 'data.service'));
 const app = express();
 const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const io = new Server(server);
 const dataServiceInstance = new dataService();
 
 // parse application/x-www-form-urlencoded
@@ -12,10 +16,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-    const obj = dataServiceInstance.processRawData(JSON.parse(JSON.stringify(req.body, null, 2)));  
+    const obj = dataServiceInstance.processRawData(JSON.parse(JSON.stringify(req.body, null, 2)));
     res.send(obj);
 });
 
-var server = app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
     console.log(`Listening on port ${port}`);
 });  
--- CM-Backend/src/service/data.service.js
+++ CM-Backend/src/service/data.service.js
@@ -1,47 +1,47 @@
 function dataService() {
-    this.dataTable=new Map();
+    this.dataTable = new Map();
 }
 
-dataService.prototype.processRawData = function(obj){
+dataService.prototype.processRawData = function (obj) {
     if (obj.spO <= 80) {
         obj.spO = Math.floor(obj.spO * (Math.random() * (1.35 - 1.25) + 1.25));
-        if(obj.spO>100){
-            obj.spO=100;
+        if (obj.spO > 100) {
+            obj.spO = 100;
         }
     }
     if (obj.heartRate >= 90) {
         obj.heartRate = Math.floor(obj.spO * (Math.random() * (0.65 - 0.55) + 0.55));
-        if(obj.heartRate <=60){
+        if (obj.heartRate <= 60) {
             obj.heartRate = Math.floor(obj.spO * (Math.random() * (1.01 - 1.005) + 1.005));
         }
     }
-    if(obj.heartRate <=60){
+    if (obj.heartRate <= 60) {
         obj.heartRate = Math.floor(obj.spO * (Math.random() * (1.01 - 1.005) + 1.005));
     }
-    obj.id=String(obj.id).replaceAll(':','');
+    obj.id = String(obj.id).replaceAll(':', '');
     this.addData(obj);
     return obj;
 }
 
-dataService.prototype.addData = function(obj){
-    if(this.dataTable.has(obj.id)){
+dataService.prototype.addData = function (obj) {
+    if (this.dataTable.has(obj.id)) {
         let dataArray = this.dataTable.get(obj.id);
         let dataObject = {
-            heartRate:obj.heartRate,
-            spO:obj.spO,
+            heartRate: obj.heartRate,
+            spO: obj.spO,
         }
-        if(dataArray.length>=3){
+        if (dataArray.length >= 3) {
             dataArray.shift();
             dataArray.push(dataObject);
-        }else{
+        } else {
             dataArray.push(dataObject);
         }
         console.log(this.dataTable);
-    }else{
+    } else {
         let dataArray = [];
         let dataObject = {
-            heartRate:obj.heartRate,
-            spO:obj.spO,
+            heartRate: obj.heartRate,
+            spO: obj.spO,
         }
         dataArray.push(dataObject);
         this.dataTable.set(obj.id, dataArray);
