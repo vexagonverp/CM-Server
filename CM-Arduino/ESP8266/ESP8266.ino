acfd7cf64a57ab53c2e08f585e45a3a91e53219e
vexagonverp <verproti80@yahoo.com.vn>
2022-04-14 08:46:34 +0700

    API/Wifi config page

--- CM-Arduino/ESP8266/ESP8266.ino
+++ CM-Arduino/ESP8266/ESP8266.ino
@@ -8,31 +8,47 @@
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+ESP8266WebServer    server(80);
+struct settings {
+  char ssid[30];
+  char password[30];
+  char server[30];
+} user_wifi = {};
 
-
-const char* ssid     = "Verproti";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "Khongbiet123";     // The password of the Wi-Fi network
 const char* serverName = "http://192.168.1.4:3000/";
 void setup() {
   //Serial.begin(115200); // got errors at this rate
   Serial.begin(57600);
   //Serial.begin(9600);
   delay(10);
-  Serial.println('\n');
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  EEPROM.begin(sizeof(struct settings) );
+  EEPROM.get( 0, user_wifi );
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(user_wifi.ssid, user_wifi.password);
+
+  byte tries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    if (tries++ > 10) {
+      WiFi.mode(WIFI_AP);
+      WiFi.softAP("Setup Portal", "password");
+      break;
+    }
   }
-  Serial.println('\n');
-  Serial.println("Connection established!");
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  if (WiFi.status() != WL_CONNECTED) {
+    server.on("/",  handlePortal);
+    server.begin();
+  } else {
+    Serial.println("Connection established!");
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  }
 }
 void loop() {
+  server.handleClient();
   StaticJsonDocument<256> doc;
   bool StringReady;
   String json;
@@ -52,14 +68,52 @@ void loop() {
         serializeJson(doc, json);
         WiFiClient client;
         HTTPClient http;
-        http.begin(client, serverName);
+        http.begin(client, user_wifi.server);
         http.addHeader("Content-Type", "application/json");
         int httpResponseCode = http.POST(json);
-
+        if (httpResponseCode != 200) {
+          Serial.println("Starting API server");
+          WiFi.mode(WIFI_AP);
+          WiFi.softAP("Setup Portal", "password");
+          server.on("/",  handlePortalApi);
+          server.begin();
+          break;
+        }
         // Free resources
         http.end();
       }
     }
   }
 
+}
+void handlePortal() {
+
+  if (server.method() == HTTP_POST) {
+
+    strncpy(user_wifi.ssid,     server.arg("ssid").c_str(),     sizeof(user_wifi.ssid) );
+    strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password) );
+    user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
+    EEPROM.put(0, user_wifi);
+    EEPROM.commit();
+
+    server.send(200,   "text/html",  "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!<br />Please restart the device.</p></main></body></html>" );
+  } else {
+
+    server.send(200,   "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'><a href='https://github.com/vexagonverp/CM-Server' style='color: #32C5FF'>CM-Server</a></p></form></main> </body></html>" );
+  }
+}
+void handlePortalApi() {
+
+  if (server.method() == HTTP_POST) {
+
+    strncpy(user_wifi.server,     server.arg("server").c_str(),     sizeof(user_wifi.server) );
+    user_wifi.server[server.arg("server").length()] = '\0';
+    EEPROM.put(0, user_wifi);
+    EEPROM.commit();
+
+    server.send(200,   "text/html",  "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!<br />Please restart the device.</p></main></body></html>" );
+  } else {
+
+    server.send(200,   "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>Api Setup</h1><br/><div class='form-floating'><label>Api</label><input type='text' class='form-control' name='server'> </div><div class='form-floating'><br/><br/><button type='submit'>Save</button><p style='text-align: right'><a href='https://github.com/vexagonverp/CM-Server' style='color: #32C5FF'>CM-Server</a></p></form></main> </body></html>" );
+  }
 }
