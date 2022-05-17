7368f71e280fd0994546ecb96555b19ddd10d05a
vexagonverp <verproti80@yahoo.com.vn>
2022-05-17 22:52:34 +0700

    Send object

--- CM-Backend/src/service/data.service.js
+++ CM-Backend/src/service/data.service.js
@@ -1,94 +1,118 @@
-const ARIMA = require('arima')
+const ARIMA = require('arima');
 
 function dataService() {
-    this.dataTable = new Map();
-    this.dataCacheSize = 100;
-    this.dataDefaultTimeInterval = 10;
-    this.dataTimeInterval = 10;
+  this.dataTable = new Map();
+  this.dataCacheSize = 100;
+  this.dataDefaultTimeInterval = 10;
+  this.dataTimeInterval = 10;
 }
 
 dataService.prototype.processRawData = function (obj) {
-    if (obj.spO <= 80) {
-        obj.spO = Math.floor(obj.spO * (Math.random() * (1.40 - 1.35) + 1.35));
-        if (obj.spO > 100) {
-            obj.spO = 100;
-        }
+  if (obj.spO <= 80) {
+    obj.spO = Math.floor(obj.spO * (Math.random() * (1.4 - 1.35) + 1.35));
+    if (obj.spO > 100) {
+      obj.spO = 100;
     }
-    if (obj.heartRate >= 90) {
-        obj.heartRate = Math.floor(obj.heartRate * (Math.random() * (0.65 - 0.55) + 0.55));
-        if (obj.heartRate <= 60) {
-            obj.heartRate = Math.floor(obj.heartRate * (Math.random() * (1.25 - 1.15) + 1.15));
-        }
-    }
+  }
+  if (obj.heartRate >= 90) {
+    obj.heartRate = Math.floor(
+      obj.heartRate * (Math.random() * (0.65 - 0.55) + 0.55)
+    );
     if (obj.heartRate <= 60) {
-        obj.heartRate = Math.floor(obj.heartRate * (Math.random() * (1.25 - 1.15) + 1.15));
+      obj.heartRate = Math.floor(
+        obj.heartRate * (Math.random() * (1.25 - 1.15) + 1.15)
+      );
     }
-    obj.id = String(obj.id).replaceAll(':', '');
-    this.addData(obj);
-    return obj;
-}
+  }
+  if (obj.heartRate <= 60) {
+    obj.heartRate = Math.floor(
+      obj.heartRate * (Math.random() * (1.25 - 1.15) + 1.15)
+    );
+  }
+  obj.id = String(obj.id).replaceAll(':', '');
+  this.addData(obj);
+  return obj;
+};
 
 dataService.prototype.addData = function (obj) {
-    if (this.dataTable.has(obj.id)) {
-        let dataArray = this.dataTable.get(obj.id);
-        let dataObject = {
-            heartRate: obj.heartRate,
-            spO: obj.spO,
-        }
-        if (dataArray.length >= this.dataCacheSize) {
-            dataArray.shift();
-            dataArray.push(dataObject);
-        } else {
-            dataArray.push(dataObject);
-        }
-        console.log(this.dataTable);
+  if (this.dataTable.has(obj.id)) {
+    let dataArray = this.dataTable.get(obj.id);
+    let dataObject = {
+      heartRate: obj.heartRate,
+      spO: obj.spO,
+    };
+    if (dataArray.length >= this.dataCacheSize) {
+      dataArray.shift();
+      dataArray.push(dataObject);
     } else {
-        let dataArray = [];
-        let dataObject = {
-            heartRate: obj.heartRate,
-            spO: obj.spO,
-        }
-        dataArray.push(dataObject);
-        this.dataTable.set(obj.id, dataArray);
+      dataArray.push(dataObject);
     }
-}
+    console.log(this.dataTable);
+  } else {
+    let dataArray = [];
+    let dataObject = {
+      heartRate: obj.heartRate,
+      spO: obj.spO,
+    };
+    dataArray.push(dataObject);
+    this.dataTable.set(obj.id, dataArray);
+  }
+};
 
 dataService.prototype.predictArima = function (id, minute) {
-    const dataArray = this.dataTable.get(id);
-    if (dataArray.length < 5) {
-        return;
+  const dataArray = this.dataTable.get(id);
+  if (dataArray.length < 5) {
+    return;
+  }
+  let heartArray;
+  let spoArray;
+  if (
+    !minute ||
+    Number(minute) > (this.dataCacheSize * this.dataTimeInterval) / 60
+  ) {
+    heartArray = dataArray
+      .slice(-10)
+      .filter((element) => element)
+      .map((element) => element.heartRate);
+    spoArray = dataArray
+      .slice(-10)
+      .filter((element) => element)
+      .map((element) => element.spO);
+  } else {
+    if (Number(minute) <= 0) {
+      minute = 1;
     }
-    let heartArray;
-    let spoArray;
-    if (!minute || Number(minute) > (this.dataCacheSize * this.dataTimeInterval)/60) {
-        heartArray = dataArray.slice(-10).filter(element => element).map(element => element.heartRate);
-        spoArray = dataArray.slice(-10).filter(element => element).map(element => element.spO);
-    } else {
-        if (Number(minute) <= 0) {
-            minute = 1;
-        }
-        let minutePercentage = Number(minute) / ((this.dataCacheSize * this.dataTimeInterval) / 60);
-        heartArray = [];
-        spoArray = [];
-        for (let i = 0; i < 1; i = i + minutePercentage) {
-            heartArray.push(Number(dataArray[Math.floor(i * (dataArray.length - 1))].heartRate));
-            spoArray.push(Number(dataArray[Math.floor(i * (dataArray.length - 1))].spO));
-        }
+    let minutePercentage =
+      Number(minute) / ((this.dataCacheSize * this.dataTimeInterval) / 60);
+    heartArray = [];
+    spoArray = [];
+    for (let i = 0; i < 1; i = i + minutePercentage) {
+      heartArray.push(
+        Number(dataArray[Math.floor(i * (dataArray.length - 1))].heartRate)
+      );
+      spoArray.push(
+        Number(dataArray[Math.floor(i * (dataArray.length - 1))].spO)
+      );
     }
-    const autoarimaHeart = new ARIMA({ auto: true, verbose: false }).fit(heartArray);
-    const autoarimaSpo = new ARIMA({ auto: true, verbose: false }).fit(spoArray);
-    //const [pred, errors] = autoarima.predict(1);
-    return autoarimaHeart.predict(1).concat(autoarimaSpo.predict(1));
-}
+  }
+  const autoarimaHeart = new ARIMA({ auto: true, verbose: false }).fit(
+    heartArray
+  );
+  const autoarimaSpo = new ARIMA({ auto: true, verbose: false }).fit(spoArray);
+  //const [pred, errors] = autoarima.predict(1);
+  return autoarimaHeart.predict(1).concat(autoarimaSpo.predict(1));
+};
 
 dataService.prototype.returnDataArray = function (id) {
-    return this.dataTable.get(id);
-}
+  return this.dataTable.get(id);
+};
 
 dataService.prototype.returnDataKey = function () {
-    return Array.from(this.dataTable.keys());
-}
+  return Array.from(this.dataTable.keys()).map((el) => {
+    return { id: el };
+  });
+};
 
 module.exports = {
-    dataService
-}
+  dataService,
+};
